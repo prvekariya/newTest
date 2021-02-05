506de078d35b65e8279c02a67f6f1882caa617d6
prvekariya <pratik.ce.268@gmail.com>
2021-02-05 12:35:39 +0530

    admin setup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,75 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<!-- begin breadcrumb -->
+<ol class="breadcrumb float-xl-right">
+  <li class="breadcrumb-item"><a href="javascript:;">Home</a></li>
+  <li class="breadcrumb-item active">Dashboard</li>
+</ol>
+<!-- end breadcrumb -->
+<!-- begin page-header -->
+<h1 class="page-header">Dashboard <small>header small text goes here...</small></h1>
+<!-- end page-header -->
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<!-- begin row -->
+<div class="row">
+  <!-- begin col-3 -->
+  <div class="col-xl-3 col-md-6">
+    <div class="widget widget-stats bg-blue">
+      <div class="stats-icon"><i class="fa fa-desktop"></i></div>
+      <div class="stats-info">
+        <h4>TOTAL VISITORS</h4>
+        <p>3,291,922</p>
+      </div>
+      <div class="stats-link">
+        <a href="javascript:;">View Detail <i class="fa fa-arrow-alt-circle-right"></i></a>
+      </div>
     </div>
+  </div>
+  <!-- end col-3 -->
+  <!-- begin col-3 -->
+  <div class="col-xl-3 col-md-6">
+    <div class="widget widget-stats bg-info">
+      <div class="stats-icon"><i class="fa fa-link"></i></div>
+      <div class="stats-info">
+        <h4>BOUNCE RATE</h4>
+        <p>20.44%</p>
+      </div>
+      <div class="stats-link">
+        <a href="javascript:;">View Detail <i class="fa fa-arrow-alt-circle-right"></i></a>
+      </div>
+    </div>
+  </div>
+  <!-- end col-3 -->
+  <!-- begin col-3 -->
+  <div class="col-xl-3 col-md-6">
+    <div class="widget widget-stats bg-orange">
+      <div class="stats-icon"><i class="fa fa-users"></i></div>
+      <div class="stats-info">
+        <h4>UNIQUE VISITORS</h4>
+        <p>1,291,922</p>
+      </div>
+      <div class="stats-link">
+        <a href="javascript:;">View Detail <i class="fa fa-arrow-alt-circle-right"></i></a>
+      </div>
+    </div>
+  </div>
+  <!-- end col-3 -->
+  <!-- begin col-3 -->
+  <div class="col-xl-3 col-md-6">
+    <div class="widget widget-stats bg-red">
+      <div class="stats-icon"><i class="fa fa-clock"></i></div>
+      <div class="stats-info">
+        <h4>AVG TIME ON SITE</h4>
+        <p>00:12:23</p>
+      </div>
+      <div class="stats-link">
+        <a href="javascript:;">View Detail <i class="fa fa-arrow-alt-circle-right"></i></a>
+      </div>
+    </div>
+  </div>
+  <!-- end col-3 -->
 </div>
+<!-- end row -->
+
 @endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Color Admin | Dashboard</title>
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+	<meta content="" name="description" />
+	<meta content="" name="author" />
+
+	<!-- ================== BEGIN BASE CSS STYLE ================== -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+	<link href="{{asset('assets/css/default/app.min.css')}}" rel="stylesheet" />
+	<!-- ================== END BASE CSS STYLE ================== -->
+
+	<!-- ================== BEGIN PAGE LEVEL STYLE ================== -->
+	<link href="{{asset('assets/plugins/jvectormap-next/jquery-jvectormap.css')}}" rel="stylesheet" />
+	<link href="{{asset('assets/plugins/bootstrap-datepicker/dist/css/bootstrap-datepicker.css')}}" rel="stylesheet" />
+	<link href="{{asset('assets/plugins/gritter/css/jquery.gritter.css')}}" rel="stylesheet" />
+	<!-- ================== END PAGE LEVEL STYLE ================== -->
+</head>
+<body>
+	<!-- begin #page-loader -->
+	<div id="page-loader" class="fade show">
+		<span class="spinner"></span>
+	</div>
+	<!-- end #page-loader -->
+
+	<!-- begin #page-container -->
+	<div id="page-container" class="fade page-sidebar-fixed page-header-fixed">
+		<!-- begin #header -->
+		<div id="header" class="header navbar-default">
+			<!-- begin navbar-header -->
+			<div class="navbar-header">
+				<a href="index.html" class="navbar-brand"><span class="navbar-logo"></span> <b>Color</b> Admin</a>
+				<button type="button" class="navbar-toggle" data-click="sidebar-toggled">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<!-- end navbar-header --><!-- begin header-nav -->
+			<ul class="navbar-nav navbar-right">
+				<li class="navbar-form">
+					<form action="" method="POST" name="search">
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="Enter keyword" />
+							<button type="submit" class="btn btn-search"><i class="fa fa-search"></i></button>
+						</div>
+					</form>
+				</li>
+				<li class="dropdown">
+					<a href="#" data-toggle="dropdown" class="dropdown-toggle f-s-14">
+						<i class="fa fa-bell"></i>
+						<span class="label">5</span>
+					</a>
+					<div class="dropdown-menu media-list dropdown-menu-right">
+						<div class="dropdown-header">NOTIFICATIONS (5)</div>
+						<a href="javascript:;" class="dropdown-item media">
+							<div class="media-left">
+								<i class="fa fa-bug media-object bg-silver-darker"></i>
+							</div>
+							<div class="media-body">
+								<h6 class="media-heading">Server Error Reports <i class="fa fa-exclamation-circle text-danger"></i></h6>
+								<div class="text-muted f-s-10">3 minutes ago</div>
+							</div>
+						</a>
+						<a href="javascript:;" class="dropdown-item media">
+							<div class="media-left">
+								<img src="../assets/img/user/user-1.jpg" class="media-object" alt="" />
+								<i class="fab fa-facebook-messenger text-blue media-object-icon"></i>
+							</div>
+							<div class="media-body">
+								<h6 class="media-heading">John Smith</h6>
+								<p>Quisque pulvinar tellus sit amet sem scelerisque tincidunt.</p>
+								<div class="text-muted f-s-10">25 minutes ago</div>
+							</div>
+						</a>
+						<a href="javascript:;" class="dropdown-item media">
+							<div class="media-left">
+								<img src="../assets/img/user/user-2.jpg" class="media-object" alt="" />
+								<i class="fab fa-facebook-messenger text-blue media-object-icon"></i>
+							</div>
+							<div class="media-body">
+								<h6 class="media-heading">Olivia</h6>
+								<p>Quisque pulvinar tellus sit amet sem scelerisque tincidunt.</p>
+								<div class="text-muted f-s-10">35 minutes ago</div>
+							</div>
+						</a>
+						<a href="javascript:;" class="dropdown-item media">
+							<div class="media-left">
+								<i class="fa fa-plus media-object bg-silver-darker"></i>
+							</div>
+							<div class="media-body">
+								<h6 class="media-heading"> New User Registered</h6>
+								<div class="text-muted f-s-10">1 hour ago</div>
+							</div>
+						</a>
+						<a href="javascript:;" class="dropdown-item media">
+							<div class="media-left">
+								<i class="fa fa-envelope media-object bg-silver-darker"></i>
+								<i class="fab fa-google text-warning media-object-icon f-s-14"></i>
+							</div>
+							<div class="media-body">
+								<h6 class="media-heading"> New Email From John</h6>
+								<div class="text-muted f-s-10">2 hour ago</div>
+							</div>
+						</a>
+						<div class="dropdown-footer text-center">
+							<a href="javascript:;">View more</a>
+						</div>
+					</div>
+				</li>
+				<li class="dropdown navbar-user">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+						<img src="{{asset('assets/img/user/user-13.jpg')}}" alt="" />
+						<span class="d-none d-md-inline">{{Auth::user()->name}}</span> <b class="caret"></b>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right">
+
+						<div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+          </div>
+				</li>
+			</ul>
+			<!-- end header-nav -->
+		</div>
+		<!-- end #header -->
+
+		<!-- begin #sidebar -->
+		<div id="sidebar" class="sidebar">
+			<!-- begin sidebar scrollbar -->
+			<div data-scrollbar="true" data-height="100%">
+				<!-- begin sidebar user -->
+				<ul class="nav">
+					<li class="nav-profile">
+						<a href="javascript:;" data-toggle="nav-profile">
+							<div class="cover with-shadow"></div>
+							<div class="image">
+								<img src="../assets/img/user/user-13.jpg" alt="" />
+							</div>
+							<div class="info">
+								<b class="caret pull-right"></b>{{Auth::user()->name}}
+								<small>Admin</small>
+							</div>
+						</a>
+					</li>
+					<li>
+						<ul class="nav nav-profile">
+							<li><a href="javascript:;"><i class="fa fa-cog"></i> Settings</a></li>
+							<li><a href="javascript:;"><i class="fa fa-pencil-alt"></i> Send Feedback</a></li>
+							<li><a href="javascript:;"><i class="fa fa-question-circle"></i> Helps</a></li>
+						</ul>
+					</li>
+				</ul>
+				<!-- end sidebar user -->
+				<!-- begin sidebar nav -->
+				<ul class="nav"><li class="nav-header">Navigation</li>
+					<!-- <li class="has-sub active">
+						<a href="javascript:;">
+							<b class="caret"></b>
+							<i class="fa fa-th-large"></i>
+							<span>Dashboard</span>
+						</a>
+						<ul class="sub-menu">
+							<li class="active"><a href="index.html">Dashboard v1</a></li>
+							<li><a href="index_v2.html">Dashboard v2</a></li>
+							<li><a href="index_v3.html">Dashboard v3</a></li>
+						</ul>
+					</li> -->
+
+					<li class="active">
+						<a href="javascript:;">
+							<i class="fa fa-th-large"></i>
+							<span>Dashboard</span>
+						</a>
+					</li>
+
+          <li>
+						<a href="{{route('job_list')}}">
+							<i class="fa fa-th-large"></i>
+							<span>Job Applications List</span>
+						</a>
+					</li>
+
+
+
+
+					<!-- begin sidebar minify button -->
+					<li><a href="javascript:;" class="sidebar-minify-btn" data-click="sidebar-minify"><i class="fa fa-angle-double-left"></i></a></li>
+					<!-- end sidebar minify button -->
+				</ul>
+				<!-- end sidebar nav -->
+			</div>
+			<!-- end sidebar scrollbar -->
+		</div>
+		<div class="sidebar-bg"></div>
+		<!-- end #sidebar -->
+
+		<!-- begin #content -->
+		<div id="content" class="content">
+			@yield('content')
+		</div>
+		<!-- end #content -->
+
+
+
+		<!-- begin scroll to top btn -->
+		<a href="javascript:;" class="btn btn-icon btn-circle btn-success btn-scroll-to-top fade" data-click="scroll-top"><i class="fa fa-angle-up"></i></a>
+		<!-- end scroll to top btn -->
+	</div>
+	<!-- end page container -->
+
+	<!-- ================== BEGIN BASE JS ================== -->
+	<script src="{{asset('assets/js/app.min.js')}}"></script>
+	<script src="{{asset('assets/js/theme/default.min.js')}}"></script>
+	<!-- ================== END BASE JS ================== -->
+
+	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
+	<script src="{{asset('assets/plugins/gritter/js/jquery.gritter.js')}}"></script>
+	<script src="{{asset('assets/plugins/flot/jquery.flot.js')}}"></script>
+	<script src="{{asset('assets/plugins/flot/jquery.flot.time.js')}}"></script>
+	<script src="{{asset('assets/plugins/flot/jquery.flot.resize.js')}}"></script>
+	<script src="{{asset('assets/plugins/flot/jquery.flot.pie.js')}}"></script>
+	<script src="{{asset('assets/plugins/jquery-sparkline/jquery.sparkline.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/jvectormap-next/jquery-jvectormap.min.js')}}"></script>
+	<script src="{{asset('assets/plugins/jvectormap-next/jquery-jvectormap-world-mill.js')}}"></script>
+	<script src="{{asset('assets/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.js')}}"></script>
+	<script src="{{asset('assets/js/demo/dashboard.js')}}"></script>
+	<!-- ================== END PAGE LEVEL JS ================== -->
+</body>
+</html>
